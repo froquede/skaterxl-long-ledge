2c1cf328d671f86d571ffa796e8236d3146674ca
Roque Francisco <roquefsd@gmail.com>
2022-03-04 17:10:14 -0300

    v0.4.0

---
+++ Assets/index.js
@@ -0,0 +1,18 @@
+const compress_images = require("compress-images");
+
+let INPUT_path_to_your_images = "./**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
+let OUTPUT_path = "test2/";
+
+compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=40-60", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    function (error, completed, statistic) {
+        console.log("-------------");
+        console.log(error);
+        console.log(completed);
+        console.log(statistic);
+        console.log("-------------");
+    }
+);
