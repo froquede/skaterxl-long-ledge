eed44b6001d3d581dc3b28aea2beb537958b33a2
Roque Francisco <roquefsd@gmail.com>
2022-04-07 11:27:14 -0300

    bump v0.7.0

--- Assets/WalkRandom.cs
+++ Assets/WalkRandom.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WalkRandom : MonoBehaviour
 {
@@ -11,6 +9,14 @@ public class WalkRandom : MonoBehaviour
     public int count = 0;
     public Vector3 last_position;
     public float last_distance = -1f;
+    public GameObject agentAnimation;
+    private Animation anim;
+    float time = 0;
+    float floor = 0;
+    bool on_floor = false;
+    bool already_up = false;
+    Camera camera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,40 +25,89 @@ public class WalkRandom : MonoBehaviour
         destination = GetRandomLocation();
         navmeshagent.SetDestination(destination);
         last_position = Vector3.zero;
+        anim = agentAnimation.GetComponent<Animation>();
+        camera = (Camera)FindObjectOfType(typeof(Camera));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (count >= 24)
+        if (!on_floor)
         {
-            Camera camera = (Camera)FindObjectOfType(typeof(Camera));
-            if (camera)
+            if (Vector3.Distance(last_position, agent.transform.position) <= navmeshagent.speed / 20 && navmeshagent.enabled && count >= 24)
             {
-                float d = Vector3.Distance(agent.transform.position, camera.transform.position);
-
-                if (d <= 8f && (last_distance > d || last_distance < 0f))
-                {
-                    last_distance = d;
-                    destination = GetRandomLocation();
-                }
-            }
-
-            if (navmeshagent.remainingDistance <= navmeshagent.stoppingDistance)
-            {
-                destination = GetRandomLocation();
+                Debug.Log(Vector3.Distance(last_position, agent.transform.position));
+                agent.transform.LookAt(new Vector3(camera.transform.position.x, agent.transform.position.y, camera.transform.position.z));
+                anim.Play("Yelling");
+                navmeshagent.enabled = false;
             }
             else
             {
-                if (Vector3.Distance(agent.transform.position, last_position) <= .1f)
+                if (!anim.isPlaying)
+                {
+                    anim.Play("Walking");
+                    navmeshagent.enabled = true;
+                }
+            }
+        }
+
+        if (navmeshagent.enabled)
+        {
+            if (count == 12 || count == 24)
+            {
+                if (camera)
+                {
+                    float d = Vector3.Distance(agent.transform.position, camera.transform.position);
+                    Debug.Log("Actual: " + d);
+                    Debug.Log("Last: " + last_distance);
+
+                    if (d <= 2 && (last_distance > d || last_distance < 0f))
+                    {
+                        last_distance = d;
+                        destination = GetRandomLocation();
+                    }
+                    if (d > 2)
+                    {
+                        last_distance = -1f;
+                    }
+                }
+            }
+            if (count >= 24)
+            {
+                if (navmeshagent.remainingDistance <= navmeshagent.stoppingDistance)
                 {
                     destination = GetRandomLocation();
                 }
+                else
+                {
+                    if (Vector3.Distance(agent.transform.position, last_position) <= .1f)
+                    {
+                        destination = GetRandomLocation();
+                    }
+                }
+                last_position = agent.transform.position;
+                count = 0;
             }
-            last_position = agent.transform.position;
-            count = 0;
+            navmeshagent.SetDestination(destination);
         }
-        navmeshagent.SetDestination(destination);
+
+        if (!anim.isPlaying && on_floor == true)
+        {
+            if (already_up == true)
+            {
+                on_floor = false;
+                already_up = false;
+                navmeshagent.enabled = true;
+                anim.Play("Walking");
+            }
+            else
+            {
+                anim.Play("Getting Up");
+                already_up = true;
+            }
+        }
+
+        time++;
         count++;
     }
 
@@ -64,5 +119,22 @@ public class WalkRandom : MonoBehaviour
         Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], 1f);
 
         return point;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Rigidbody rb = collider.GetComponent<Rigidbody>();
+        if (rb)
+        {
+            Vector3 localVel = agent.transform.InverseTransformDirection(rb.velocity);
+            if (localVel.x > 0.1f || localVel.y > 0.1f || localVel.z > 0.1f)
+            {
+                navmeshagent.enabled = false;
+                agent.transform.LookAt(new Vector3(rb.transform.position.x, agent.transform.position.y, rb.transform.position.z));
+                if (anim) anim.Play("Falling Down");
+                on_floor = true;
+                floor = time;
+            }
+        }
     }
 }
--- Assets/index.js
+++ Assets/index.js
@@ -1,7 +1,7 @@
 const compress_images = require("compress-images");
 
 let INPUT_path_to_your_images = "./**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
-let OUTPUT_path = "test2/";
+let OUTPUT_path = "compressed/";
 
 compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
     { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
